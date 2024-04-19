c6e1aee2f42aea6c30f44127560c2d9a202ccfca
kamiliaa2 <k_sifouane@estin.dz>
2024-04-19 16:30:48 +0100

    update

--- src/pages/profilepage/profile/profile.jsx
+++ src/pages/profilepage/profile/profile.jsx
@@ -13,39 +13,32 @@ const Profile = () => {
         </div>
         <div className="my-profile">
           <h1>my peofile</h1>
-          <br />
+          
           <hr />
 
           <div className="formulaire">
+        
             <div className="infos">
+              <div className=" image-container">
               <img
                 src="Rectangle 499.png"
                 alt="C:\Users\NetinfoTALI\Desktop\project\public\Rectangle 499.png"
-                width={400}
-                height={400}
+                width={300}
+                height={360}
               />
-
-              <ul>
-                <h2>Contact Information</h2>
-                <br />
-                <li>
-                  <strong>Age:</strong> 26yo
-                </li>
-                <li>
-                  <strong>Gender:</strong> Female
-                </li>
-                <li>
-                  <strong>Birthday:</strong> 21 August, 1997
-                </li>
-                <li>
-                  <strong>Civil Status:</strong> Single
-                </li>
-              </ul>
-
-              <ul className="right">
-                <li>
-                  <strong>Occupation:</strong> HR Manager
-                </li>
+              </div>
+              <div className=" contacts-infos">
+              <h2>Contact Information</h2>
+              <div className=" infos-fields">
+                <ul className="contactinf">
+                  <li> <strong>Age:</strong> 26yo</li>
+                  <li><strong>Gender:</strong> Female</li>
+                  <li><strong>Birthday:</strong> 21 August, 1997</li>
+                  <li><strong>Civil Status:</strong> Single</li>
+                </ul>
+                <div className="occupation">
+                <ul >
+                  <li><strong>Occupation:</strong> HR Manager</li>
                 <li>
                   <strong>Mobile:</strong> 0555051625
                 </li>
@@ -56,6 +49,10 @@ const Profile = () => {
                   <strong>Address:</strong> Algiers, Ouled Fayet
                 </li>
               </ul>
+                </div>
+               
+              </div>
+              </div>
             </div>
           </div>
         </div>
